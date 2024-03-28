90ca503d7cc0335a5dd5f7e246baaf48c1baaad4
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2024-03-29 00:28:05 +0600

    tailwind-config

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
 import MenuBar from "../menu/MenuBar";
+import { Col, Row } from "antd";
 
 const RootLayout = () => {
   return (
     <>
-      <MenuBar />
-      <Outlet />
+      <div className="xl:container container">
+        <Row>
+          <Col className="xl:w-1/5">
+            <MenuBar />
+          </Col>
+          <Col className="xl:w-4/5">
+            <Outlet />
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
