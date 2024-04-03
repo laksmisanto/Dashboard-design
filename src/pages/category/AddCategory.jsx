6b094bb1096fefc11508e2d48e51a4c44c91fdc4
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2024-04-04 02:28:18 +0600

    adduser-addproduct-page-create

--- src/pages/category/AddCategory.jsx
+++ src/pages/category/AddCategory.jsx
@@ -1,7 +1,136 @@
+import {
+  UserOutlined,
+  AlignLeftOutlined,
+  MinusOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import { Input, Button, Upload, Image } from "antd";
+import { useState } from "react";
+
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 const AddCategory = () => {
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    details: "",
+    categoryImage: "",
+    slug: "",
+  });
+
+  const handleInputValues = (e) => {
+    const { name, value } = e.target;
+    setInputValues((prevValue) => ({ ...prevValue, [name]: value }));
+  };
+
+  const handleAddCategory = async () => {
+    console.log("click hoise");
+  };
+
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileList, setFileList] = useState([]);
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+  };
+  const handleChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const uploadButton = (
+    <button
+      style={{
+        border: 0,
+        background: "none",
+      }}
+      type="button"
+    >
+      <PlusOutlined />
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </button>
+  );
+
   return (
     <>
-      <h2>This is add category page</h2>
+      <div>
+        <Input
+          size="large"
+          className="mb-5"
+          placeholder="Category name"
+          name="name"
+          onChange={(e) => handleInputValues(e)}
+          value={inputValues.name}
+          prefix={<UserOutlined />}
+        />
+      </div>
+      <div>
+        <Input
+          size="large"
+          className="mb-5"
+          placeholder="Category details"
+          name="details"
+          onChange={(e) => handleInputValues(e)}
+          value={inputValues.details}
+          prefix={<AlignLeftOutlined />}
+        />
+      </div>
+      <div>
+        <Upload
+          // action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={handlePreview}
+          name="categoryImage"
+          onChange={handleChange}
+          value={inputValues.categoryImage}
+        >
+          {fileList.length >= 5 ? null : uploadButton}
+        </Upload>
+        {previewImage && (
+          <Image
+            wrapperStyle={{
+              display: "none",
+            }}
+            preview={{
+              visible: previewOpen,
+              onVisibleChange: (visible) => setPreviewOpen(visible),
+              afterOpenChange: (visible) => !visible && setPreviewImage(""),
+            }}
+            src={previewImage}
+          />
+        )}
+      </div>
+      <div>
+        <Input
+          size="large"
+          className="mb-5"
+          placeholder="Category slug"
+          prefix={<MinusOutlined />}
+          name="slug"
+          onChange={(e) => handleInputValues(e)}
+          value={inputValues.slug}
+        />
+      </div>
+      <div>
+        <Button size="large" type="primary" onClick={handleAddCategory}>
+          Primary
+        </Button>
+      </div>
     </>
   );
 };
--- src/pages/user/AddUser.jsx
+++ src/pages/user/AddUser.jsx
@@ -1,26 +1,217 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Button, Col, Input, Row } from "antd";
+import { Button, Col, Input, Row, Select, Typography } from "antd";
+import { useState } from "react";
 
 const AddUser = () => {
+  const [addUsers, setAddUsers] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    addressOne: "",
+    addressTwo: "",
+    city: "",
+    postCode: "",
+    division: "",
+    district: "",
+    role: "",
+  });
+  const handleAddUsers = (e) => {
+    const { name, value } = e.target;
+    setAddUsers((prevValue) => ({ ...prevValue, [name]: value }));
+  };
+
+  const handleChange = (value) => {
+    // console.log(`selected ${value}`);
+  };
+
   return (
     <>
-      <Row className="h-screen justify-center items-center">
-        <Col className="w-2/5 text-center">
-          <Input
-            className="mb-3"
-            placeholder="User name"
-            prefix={<UserOutlined />}
-          />
-          <Input
-            className="mb-5"
-            placeholder="Email"
-            prefix={<UserOutlined />}
-          />
-          <Button type="button" className="bg-orange-700 text-white">
-            Submit
-          </Button>
+      <Row gutter={8}>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>First name</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="first name"
+              name="firstName"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.firstName}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>Last name</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="last name"
+              name="lastName"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.lastName}
+              prefix={<UserOutlined />}
+            />
+          </div>
         </Col>
       </Row>
+      <Row gutter={8}>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>Email address</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="email address"
+              name="email"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.email}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          {" "}
+          <div>
+            <Typography.Title level={5}>Phone number</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="phone number"
+              name="phoneNumber"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.phoneNumber}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={8}>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>Address one</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="address one"
+              name="addressOne"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.addressOne}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          {" "}
+          <div>
+            <Typography.Title level={5}>Address two</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="address two"
+              name="addressTwo"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.addressTwo}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={8}>
+        <Col span={12}>
+          {" "}
+          <div>
+            <Typography.Title level={5}>City</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="city"
+              name="city"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.city}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>Post code</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="post code"
+              name="postCode"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.postCode}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={8}>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>Division</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="division"
+              name="division"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.division}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <Typography.Title level={5}>District</Typography.Title>
+            <Input
+              size="large"
+              className="mb-2"
+              placeholder="district"
+              name="district"
+              onChange={(e) => handleAddUsers(e)}
+              value={addUsers.district}
+              prefix={<UserOutlined />}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <Typography.Title level={5}>Role</Typography.Title>
+          <Select
+            defaultValue="select role"
+            style={{
+              width: "100%",
+            }}
+            size="large"
+            onChange={handleChange}
+            options={[
+              {
+                value: "Admin",
+                label: "Admin",
+              },
+              {
+                value: "Editor",
+                label: "Editor",
+              },
+              {
+                value: "Customer",
+                label: "Customer",
+              },
+            ]}
+          />
+        </Col>
+      </Row>
+      <div className="mt-8">
+        <Button size="large" type="primary">
+          Primary
+        </Button>
+      </div>
     </>
   );
 };
